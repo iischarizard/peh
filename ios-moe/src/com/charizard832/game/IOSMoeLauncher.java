d7063e0d53d0f4b01a3a882211ab5209652e99d9
admin <zlnorman10@gmail.com>
2017-10-08 23:49:20 -0500

    Works on ios but no ads yet :<

--- ios-moe/src/com/charizard832/game/IOSMoeLauncher.java
+++ ios-moe/src/com/charizard832/game/IOSMoeLauncher.java
@@ -2,12 +2,17 @@ package com.charizard832.game;
 
 import com.badlogic.gdx.backends.iosmoe.IOSApplication;
 import com.badlogic.gdx.backends.iosmoe.IOSApplicationConfiguration;
+
 import org.moe.natj.general.Pointer;
 import com.charizard832.game.LegendGame;
 
 import apple.uikit.c.UIKit;
 
 public class IOSMoeLauncher extends IOSApplication.Delegate implements ActionResolver{
+
+    //private GADInterstitial interstitial;
+    private IOSApplication application;
+
 
     protected IOSMoeLauncher(Pointer peer) {
         super(peer);
@@ -17,11 +22,24 @@ public class IOSMoeLauncher extends IOSApplication.Delegate implements ActionRes
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
         config.useAccelerometer = false;
-        return new IOSApplication(new LegendGame(this), config);
+        //GADMobileAds.configureWithApplicationID("ca-app-pub-3435145263390263~2671531433");
+        //interstitial = GADInterstitial.alloc().initWithAdUnitID("ca-app-pub-3435145263390263/8241074766");
+        //interstitial.loadRequest(GADRequest.request());
+
+        application = new IOSApplication(new LegendGame(this), config);
+        return application;
+
     }
 
     public static void main(String[] argv) {
         UIKit.UIApplicationMain(0, null, null, IOSMoeLauncher.class.getName());
     }
-    public void showOrLoadInterstital(){}
+    public void showOrLoadInterstital(){
+        /*if(interstitial.isReady()){
+            interstitial.presentFromRootViewController(application.getUIViewController());
+        }else{
+            interstitial.loadRequest(GADRequest.request());
+        }*/
+
+    }
 }
